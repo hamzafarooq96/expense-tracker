1e5f8ab7892b4a4a55e0edd0a930bfc587afa9cb
Hamza Farooq <51108114+hamzafarooq96@users.noreply.github.com>
2020-10-25 05:47:02 +0500

    Saturday Work
    My Saturday Commit Work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./Header";
+import Balance from "./Balance";
+import AccountSummry from "./AccountSummry";
+import AccountTrans from "./AccountTrans";
+import AddTransaction from "./AddTransaction";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header/>
+    <div className="container">
+      <Balance/>
+      <AccountSummry/>
+      <AccountTrans/>
+      <AddTransaction/>
+    </div>
     </div>
   );
 }
