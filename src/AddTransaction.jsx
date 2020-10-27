3c384943d63c0d8db1e4ecd0e83124af217d4199
Hamza Farooq <51108114+hamzafarooq96@users.noreply.github.com>
2020-10-27 17:27:38 +0500

    Creating Context
    First Context Creation

--- src/AddTransaction.jsx
+++ src/AddTransaction.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransaction(){
+
+    const [description, setDescription] = useState();
+    const [transactionAmount, setTransactionAmount] = useState();
 
 return(
 
@@ -15,14 +18,23 @@ return(
 
 <label htmlFor="description">Description</label>
 
-<input type="text" id="description" placeholder="Detail of transaction"/>
+<input type="text" 
+id="description" 
+placeholder="Detail of transaction" 
+value={description} 
+onChange= {(e) => setDescription(e.target.value) }/>
 
 </div>
 
 <div className="form-control">
 
 <label htmlFor="transactionamount">Transaction Amount</label>
-<input type="number" id="transactionamount" placeholder="Enter transaction Amount"/>
+<input type="number" 
+id="transactionamount" 
+placeholder="Enter transaction Amount" 
+value={transactionAmount}
+onChange= {(e) => setTransactionAmount(e.target.value) }
+/>
 
 </div>
 <button className="btn">Add Transaction</button>
---
+++ src/GlobalState.js
@@ -0,0 +1,13 @@
+import React, { createContext, useReducer } from "react";
+
+//Create initial State
+
+const initialState = {
+
+transaction: [
+
+   {}
+
+]
+
+}
